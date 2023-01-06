31d22096d8a78af98f80945173428bea191608a8
“Andrii <yanak1984@gmail.com>
2023-01-06 10:58:47 +0200

    Fix returning the new user with the same wallet

--- components/oauth/express.js
+++ components/oauth/express.js
@@ -17,7 +17,7 @@ module.exports = function(app, api, User, ens) {
   app.get('/oauth/user', authenticate(), async function(req, res) {
     const ens_name = ens ? await ens.reverse(req.user.User.address) : null
 
-    return ens_name ? res.json(Object.assign(req.user.User, {ens: ens_name})) : res.json(Object.assign(req.user.User, {name: req.user.User.address, nickname: req.user.User.address}))
+    return ens_name ? res.json(Object.assign(req.user.User, {ens: ens_name})) : res.json(Object.assign(req.user.User, {sub: req.user.User.address, name: req.user.User.address, nickname: req.user.User.address}))
   })
 
   app.all('/oauth/token', function(req, res, next) {
